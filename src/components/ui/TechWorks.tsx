63e80a42b0ac3bf9e80992b8b0e8bdd29c1625df
Sereja_Lebeda <lserg97@gmail.com>
2026-09-18 10:42:42 +0300

    Feat: Coming soon page

---
+++ src/components/ui/TechWorks.tsx
@@ -0,0 +1,18 @@
+function TechWorks() {
+  return (
+    <div
+      className="w-full h-dvh xl:h-full
+      flex justify-center items-center
+    "
+    >
+      <img
+        src="public/TempTechWorks.png"
+        alt=""
+        className="object-contain w-200 max-w-full
+h-200 max-h-full"
+      />
+    </div>
+  );
+}
+
+export default TechWorks;
